d37bd4e99d2fe3b82aa13a32c170164fbbbcddb2
unknown <apoorvapaudel2062@gmail.com>
2026-03-04 14:10:41 +0545

    to add items

--- js/app.js
+++ js/app.js
@@ -1,33 +1,57 @@
 import { groceryItems } from "./data.js";
 import { createItems } from "./items.js";
+import { createForm } from "./form.js";
 
 let items = groceryItems;
+let startEditGlobal;
 
-// Render App
 function render() {
   const itemsContainer = document.querySelector(".items");
   itemsContainer.innerHTML = "";
 
-  const itemsElements = createItems(items, removeItem, editCompleted);
+  const itemsElements = createItems(
+    items,
+    removeItem,
+    editCompleted,
+    startEditGlobal,
+  );
+
   itemsContainer.appendChild(itemsElements);
 }
 
-// Remove Item
-function removeItem(itemId) {
-  items = items.filter((item) => item.id !== itemId);
-
-  alert("Item Deleted Successfully!");
-
+function addItem(name) {
+  items.push({
+    id: Date.now(),
+    name,
+    completed: false,
+  });
   render();
 }
 
-// Toggle Completed
-export function editCompleted(itemId) {
+function updateItem(id, newName) {
   items = items.map((item) =>
-    item.id === itemId ? { ...item, completed: !item.completed } : item,
+    item.id === id ? { ...item, name: newName } : item,
   );
   render();
 }
 
-// Initialize
+function removeItem(id) {
+  alert("Item Deleted Successfully!");
+  items = items.filter((item) => item.id !== id);
+  render();
+}
+
+function editCompleted(id) {
+  items = items.map((item) =>
+    item.id === id ? { ...item, completed: !item.completed } : item,
+  );
+  render();
+}
+
+// Initialize Form
+const { form, startEdit } = createForm(addItem, updateItem);
+startEditGlobal = startEdit;
+
+document.getElementById("form-container").appendChild(form);
+
 render();
